474d218e90d28cd887749ed0973fc4a14dfa8188
ChaosswordLP <48867311+ChaosswordLP@users.noreply.github.com>
2021-01-23 16:01:57 +0100

    EditHistory added for changes in history & undo command added

---
+++ src/Chaos/ChaosEdit/CommandExecutor/Undo.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace Chaos\ChaosEdit\CommandExecutor;
+
+use Chaos\ChaosEdit\EditHistory;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+
+class Undo implements CommandExecutor {
+
+	public static array $positions = [];
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+		$reset = 20;
+		if(isset($args[0])){
+			$reset = $args[0];
+			return true;
+		}else{
+			if(count(self::$positions) === 0){
+				$sender->sendMessage("§dThere's nothin to undo");
+				return true;
+			}
+			if(count(self::$positions) === $reset){
+				array_shift(self::$positions);
+			}
+			$lastEditHistory = array_pop(self::$positions);
+			$world = $sender->getServer()->getWorldManager()->getWorldByName($lastEditHistory->getWorldName());
+			if($world === null){
+				$sender->sendMessage("§cWorld is not loaded");
+				return true;
+			}
+			foreach($lastEditHistory->yieldBlockHistory() as [$x, $y, $z, $block]){
+				$world->setBlockAt($x, $y, $z, $block, false);
+			}
+
+			$sender->sendMessage("§4... you overwrote history...");
+			return true;
+		}
+	}
+}
---
+++ src/Chaos/ChaosEdit/EditHistory.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types = 1);
+
+namespace Chaos\ChaosEdit;
+
+use Generator;
+use pocketmine\block\Block;
+
+class EditHistory {
+
+	private string $worldName;
+	private array $positions = [];
+
+	public function __construct(string $worldName){
+		$this->worldName = $worldName;
+	}
+
+	public function getWorldName(): string{
+		return $this->worldName;
+	}
+
+	public function setBlockHistory(int $x, int $y, int $z, Block $block): void{
+		if(isset($this->positions["$x;$y;$z"])){
+			throw new \InvalidArgumentException("Cannot overwrite history");
+		}
+		$this->positions["$x;$y;$z"] = $block;
+	}
+
+	public function getBlockAmount(): int{
+		return count($this->positions);
+	}
+
+	public function yieldBlockHistory(): Generator{
+		foreach($this->positions as $posData => $block){
+			$posData = explode(";", $posData);
+			yield [
+				(int) $posData[0],
+				(int) $posData[1],
+				(int) $posData[2],
+				$block
+			];
+		}
+	}
+}
